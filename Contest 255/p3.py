f28757bb4474cdbf6381ee23e50ec8dd5e208ffa
mblasterx <valentin@mathlinks.ro>
2021-09-02 11:29:59 -0700

    Some cleanup

--- Contest 255/p3.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def minimizeTheDifference(self, mat, target):
-        """
-        :type mat: List[List[int]]
-        :type target: int
-        :rtype: int
-        """
-        
