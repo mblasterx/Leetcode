0318324df197e1a436f37b5798f0de6dfc7e23dd
mblasterx <valentin@mathlinks.ro>
2021-09-03 00:03:56 -0700

    figured out a different solution

--- p69.py
+++ p69.py
@@ -2,13 +2,32 @@ class Solution:
     '''https://leetcode.com/problems/sqrtx/
     '''
     def mySqrt(self, x: int) -> int:
-        '''Given a non-negative integer x, compute and return the square root of x.
-        Since the return type is an integer, the decimal digits are truncated, and only the integer part of the result is returned.
+        '''Simple linear search
         '''
         i = 1
         while i*i <= x:
             i+=1
         return i-1
+
+    def mySqrt(self, x: int) -> int:
+        '''Divide and conquer
+        '''
+        if x <= 1: 
+            return x
+        
+        left, right = 1, x // 2 + 1
+        while left < right:
+            mid = (left + right) // 2   # adjust the window
+            square = mid * mid         
+            # we check for squares, or move the window and shrink it in half
+            if square == x:     
+                return mid
+            if square > x: 
+                right = mid
+            else: 
+                left = mid + 1
+        
+        return right - 1
 
     def test(self) -> None:
         testCases = {
