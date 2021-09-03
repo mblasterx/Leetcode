752a9e1c6d5c8322189fb1950f45ef0c106c4c86
mblasterx <valentin@mathlinks.ro>
2021-09-03 08:43:59 -0700

    small problem

---
+++ p28.py
@@ -0,0 +1,21 @@
+class Solution:
+    '''https://leetcode.com/problems/implement-strstr/
+    '''
+    def strStr(self, haystack: str, needle: str) -> int:
+        '''The str.find() method does this exact thing
+        '''
+        return haystack.find(needle)
+        
+    def test(self) -> None:
+        testCases = {
+            ('hello', 'll')     :2,
+            ('aaaaa', 'bba')    :-1,
+            ('', '')            :0,
+            ('aaaa', '')        :0,
+        }
+        for (haystack, needle),val in testCases.items():
+            assert self.strStr(haystack, needle) == val, (haystack, needle, val)
+        print('All tests passed')
+    
+s = Solution()
+s.test()    
