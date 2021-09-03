9b74d403b988e45414cce396af641d6c98d68eca
mblasterx <valentin@mathlinks.ro>
2021-09-02 23:53:17 -0700

    99.7% better memory, 18% speed

---
+++ p69.py
@@ -0,0 +1,26 @@
+class Solution:
+    '''https://leetcode.com/problems/sqrtx/
+    '''
+    def mySqrt(self, x: int) -> int:
+        '''Given a non-negative integer x, compute and return the square root of x.
+        Since the return type is an integer, the decimal digits are truncated, and only the integer part of the result is returned.
+        '''
+        i = 0
+        while i*i <= x:
+            i+=1
+        return i-1
+
+    def test(self) -> None:
+        testCases = {
+            4       :2,
+            1       :1,
+            8       :2,
+            15      :3,
+            9       :3,
+        }
+        for key,val in testCases.items():
+            assert self.mySqrt(key) == val 
+        print('All tests passed')
+    
+s = Solution()
+s.test()
